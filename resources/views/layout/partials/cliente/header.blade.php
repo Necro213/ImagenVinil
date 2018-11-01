ed87d9c4003fa0b9bcde1302a5811008959adffb
Alexis <necro213a@gmail.com>
2018-10-31 17:51:12 -0700

    rutas colocadas funcionales

--- resources/views/layout/partials/cliente/header.blade.php
+++ resources/views/layout/partials/cliente/header.blade.php
@@ -3,20 +3,19 @@
         <div class="row">
             <div class="col">
                 <div class="header_content d-flex flex-row align-items-center justify-content-start trans_400">
-                    <div class="logo"><a href="#">Host<span>Space</span></a></div>
+                    <div class="logo"><a href="#">Imagen En Vinil</a></div>
                     <nav class="main_nav ml-auto mr-auto">
                         <ul class="d-flex flex-row align-items-center justify-content-start">
-                            <li class="active"><a href="index.html">Home</a></li>
-                            <li><a href="about.html">About us</a></li>
-                            <li><a href="services.html">Services</a></li>
-                            <li><a href="blog.html">News</a></li>
-                            <li><a href="contact.html">Contact</a></li>
+                            <li><a href="{{route('cliente.index')}}">Home</a></li>
+                            <li><a href="{{route('cliente.about')}}">About us</a></li>
+                            <li><a href="{{route('cliente.services')}}">Services</a></li>
+                            <li><a href="{{route('cliente.elements')}}">News</a></li>
+                            <li><a href="{{route('cliente.contact')}}">Contact</a></li>
                         </ul>
                     </nav>
                     <div class="log_reg">
                         <div class="log_reg_content d-flex flex-row align-items-center justify-content-start">
-                            <div class="login log_reg_text"><a href="#">Login</a></div>
-                            <div class="register log_reg_text"><a href="#">Register</a></div>
+
                         </div>
                     </div>
                     <div class="hamburger ml-auto"><i class="fa fa-bars" aria-hidden="true"></i></div>
@@ -38,11 +37,11 @@
     </div>
     <nav class="menu_nav">
         <ul>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="about.html">About us</a></li>
-            <li><a href="services.html">Services</a></li>
-            <li><a href="blog.html">News</a></li>
-            <li><a href="contact.html">Contact</a></li>
+            <li><a href="{{route('cliente.index')}}">Home</a></li>
+            <li><a href="{{route('cliente.about')}}">About us</a></li>
+            <li><a href="{{route('cliente.services')}}">Services</a></li>
+            <li><a href="{{route('cliente.elements')}}">News</a></li>
+            <li><a href="{{route('cliente.contact')}}">Contact</a></li>
         </ul>
     </nav>
 </div>
