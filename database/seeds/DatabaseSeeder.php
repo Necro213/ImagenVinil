3df45c9c914d2a14f86f0272ae235fd9de0a60a8
Alexis <necro213a@gmail.com>
2018-11-12 19:49:20 -0700

    seeder estaciones

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,12 +12,48 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
+        /*DB::table('users')->insert([
             'nombre' => 'Alexis Adrian',
             'apellidos' => 'Bogarin Santana',
             'username' => 'Necro',
             'password' => bcrypt('1234'),
             'apikey' => bcrypt('Necro')
+        ]);*/
+
+        DB::table('estacion_config')->insert([
+            'image' => 'primavera.png',
+            'estacion' => 'Primavera',
+            'dia' => 22,
+            'mes' => 03,
+            'diaf' => 21,
+            'mesf' => 06
+        ]);
+
+        DB::table('estacion_config')->insert([
+            'image' => 'verano.png',
+            'estacion' => 'Verano',
+            'dia' => 22,
+            'mes' => 06,
+            'diaf' => 21,
+            'mesf' => 9
+        ]);
+
+        DB::table('estacion_config')->insert([
+            'image' => 'otono.png',
+            'estacion' => 'Otono',
+            'dia' => 22,
+            'mes' => 9,
+            'diaf' => 21,
+            'mesf' => 12
+        ]);
+
+        DB::table('estacion_config')->insert([
+            'image' => 'invierno.png',
+            'estacion' => 'Invierno',
+            'dia' => 22,
+            'mes' => 12,
+            'diaf' => 21,
+            'mesf' => 3
         ]);
     }
 }
